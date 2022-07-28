38172639cdd4915ef7e4f2cdd0845e3a4ae52e9b
0x1772 <realhateux@outlook.com>
2022-07-28 21:32:07 +0300

    sign_in.dart

--- lib/ui/screen/sign_in.dart
+++ lib/ui/screen/sign_in.dart
@@ -1,2 +1,151 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {}
+}
+
+class _SignInPageState extends State<SignInPage> {
+  static DateTime now = DateTime.now();
+  String formattedTime = DateFormat.jm().format(now);
+  String formattedDate = DateFormat('MMM d, yyyy | EEEEEE').format(now);
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Theme.of(context).backgroundColor,
+      child: SafeArea(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: SvgPicture.asset(mainBanner),
+            ),
+          )
+        Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(18),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    _topContent(),
+                    _centerContent(),
+                    _bottomContent()
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+    Widget _topContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const SizedBox(
+          height: 18,
+        ),
+        Row(
+          children: <Widget>[
+            Text(
+              formattedTime,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const SizedBox(
+              width: 30,
+            ),
+            SvgPicture.asset(cloud),
+            const SizedBox(
+              width: 8,
+            ),
+            Text(
+              '34° C',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 2,
+        ),
+        Text(
+          formattedDate,
+          style: Theme.of(context).textTheme.bodyText2,
+        )
+      ],
+    );
+  }
+
+  Widget _centerContent() {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SvgPicture.asset(logo),
+          const SizedBox(
+            height: 18,
+          ),
+          Text(
+            'SmileWallet',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          Text(
+            'Open An Account For Digital  E-Wallet Solutions.\nInstant Payouts. \n\nJoin For Free.',
+            style: Theme.of(context).textTheme.bodyText1,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _bottomContent() {
+    return Column(
+      children: <Widget>[
+        MaterialButton(
+          elevation: 0,
+          color: const Color(0xFFFFAC30),
+          height: 50,
+          minWidth: 200,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const DrawerPage(),
+              ),
+            );
+          },
+          child: Text(
+            'Sign in',
+            style: Theme.of(context).textTheme.button,
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Text(
+          'Create an Account',
+          style: Theme.of(context).textTheme.bodyText2,
+        )
+      ],
+    );
+  }
+}
